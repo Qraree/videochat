ff984f165150f82883961fa0bf51b50f70c05b01
Qraree <metbezu@gmail.com>
2023-06-21 20:46:14 +0300

    websocket fix

--- client_next/src/socket.ts
+++ client_next/src/socket.ts
@@ -2,4 +2,4 @@ import { io } from 'socket.io-client';
 
 const URL = process.env.NEXT_PUBLIC_SERVER_URL || "localhost:2000";
 
-export const socket = io(URL);
+export const socket = io(URL, {transports: ["websocket"]});
